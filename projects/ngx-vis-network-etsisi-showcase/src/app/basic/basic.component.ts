17c4174cead745a8e3a5049e2786224ab0491bf7
jrodva <informatiloco@gmail.com>
2020-10-09 18:45:00 +0200

    Adding functionality to the sliders in the basic component

--- projects/ngx-vis-network-etsisi-showcase/src/app/basic/basic.component.ts
+++ projects/ngx-vis-network-etsisi-showcase/src/app/basic/basic.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataSet, Edge, NgxVisNetworkEtsisiService, Node, Options } from 'ngx-vis-network-etsisi';
 import { graphNetworkEdges, graphNetworkNodes, graphNetworkOptions } from '../../assets/data';
 
@@ -7,39 +7,82 @@ import { graphNetworkEdges, graphNetworkNodes, graphNetworkOptions } from '../..
   templateUrl: './basic.component.html',
   styleUrls: ['./basic.component.scss']
 })
-export class BasicComponent {
+export class BasicComponent implements OnInit {
   id: string;
   nodes: DataSet<Node>;
   edges: DataSet<Edge>;
   options: Options;
-  showNodes: boolean;
   showEdges: boolean;
   showIcons: boolean;
   showLabels: boolean;
   showButtons: boolean;
-  @ViewChild('etsisiGraphNetwork', { read: true, static: false }) container: ElementRef;
 
   constructor(private ngxVisNetworkEtsisiService: NgxVisNetworkEtsisiService) {
+    console.log('constructor');
     this.id = 'etsisiGraphNetwork';
+  }
+
+  ngOnInit() {
+    console.log('on init ');
     this.nodes = new DataSet<Node>(graphNetworkNodes);
     this.edges = new DataSet<Edge>(graphNetworkEdges);
-    this.showNodes = true;
+    this.options = { ...graphNetworkOptions };
     this.showEdges = true;
     this.showIcons = true;
     this.showLabels = true;
     this.showButtons = true;
-    this.options = { ...graphNetworkOptions };
-  }
-
-  activeNodes(activeNodes: boolean) {
-    this.showNodes = !this.showNodes;
-    if (activeNodes) {
-      this.ngxVisNetworkEtsisiService.updateNode(this.id, [{ id: 4, label: 'Codessss' }]);
-    }
   }
 
   isGraphNetworkReady() {
-    console.log('Graph network is ready');
-    this.ngxVisNetworkEtsisiService.create(this.container.nativeElement, this.id, this.nodes, this.edges, this.options);
+    console.log('The graph is loaded');
+  }
+
+  hideShowButtons() {
+    console.log('hideShowButtons ');
+    this.showButtons = !this.showButtons;
+    this.ngxVisNetworkEtsisiService.setOptions(this.id, {
+      ...graphNetworkOptions,
+      interaction: {
+        navigationButtons: this.showButtons
+      }
+    });
+  }
+
+  hideShowEdges() {
+    console.log('hideShowEdges ');
+    this.showEdges = !this.showEdges;
+    if (this.showEdges) {
+      this.ngxVisNetworkEtsisiService.setData(this.id, { edges: this.edges, nodes: this.nodes });
+    } else {
+      this.ngxVisNetworkEtsisiService.setData(this.id, { nodes: this.nodes });
+    }
+  }
+
+  hideShowLabels() {
+    console.log('hideShowLabels ');
+    this.showLabels = !this.showLabels;
+    if (this.showLabels) {
+      this.ngxVisNetworkEtsisiService.setData(this.id, { edges: this.edges, nodes: this.nodes });
+    } else {
+      const auxNodes = this.nodes.map(node => {
+        node.label = '';
+        return node;
+      });
+      this.ngxVisNetworkEtsisiService.setData(this.id, { edges: this.edges, nodes: auxNodes });
+    }
+  }
+
+  hideShowIcons() {
+    console.log('hideShowIcons ');
+    this.showIcons = !this.showIcons;
+    if (this.showIcons) {
+      this.ngxVisNetworkEtsisiService.setData(this.id, { edges: this.edges, nodes: this.nodes });
+    } else {
+      const auxEdges = this.edges.map(edge => {
+        edge.arrows = null;
+        return edge;
+      });
+      this.ngxVisNetworkEtsisiService.setData(this.id, { edges: auxEdges, nodes: this.nodes });
+    }
   }
 }
